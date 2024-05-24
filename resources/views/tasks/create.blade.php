546894be1153c06a837651d20af6fec7a798e6ea
jonivalo <jonivirvalo@gmail.com>
2024-05-24 14:23:24 +0300

    Update create.blade.php

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Create Task for Project: ') }} {{ $project->name }}
+            {{ __('Create task for project: ') }} {{ $project->name }}
         </h2>
     </x-slot>
 
